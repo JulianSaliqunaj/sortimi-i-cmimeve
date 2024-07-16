989549209056eefafb4af6cf9e7c4f935ad6ada9
JulianSaliqunaj <juliansaliqunaj8@gmail.com>
2024-07-16 11:58:36 +0200

    Add files via upload

---
+++ index11.py
@@ -0,0 +1,9 @@
+produktet = {
+    "tv" : 300,
+    "mouse" : 5,
+    "laptop" : 1000,
+    "keyboard" : 50
+}
+print(sorted(produktet.values()))
+print(f"produkti me qmimin me te vogel eshte", ())
+
